4069f0ff8de683e620c21cd7f151126608796279
Kamran Shaghaghi <76891849+Kam4280@users.noreply.github.com>
2026-07-25 14:50:21 +0000

    fix(network): correct nat_ip_allocate_option schema value to AUTO_ONLY

--- modules/01-vpc-network/main.tf
+++ modules/01-vpc-network/main.tf
@@ -65,7 +65,7 @@ resource "google_compute_router_nat" "nat" {
   project                            = var.project_id
   region                             = var.region
   router                             = google_compute_router.router.name
-  nat_ip_allocate_option             = "AUTO_ALLOCATE"
+  nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
   log_config {
